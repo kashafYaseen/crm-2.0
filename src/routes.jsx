4180746b47c66b1ae7874fe8c7aeb480c36158e7
Zoha Qayyum <zoha17qayyum@gmail.com>
2023-07-05 12:25:54 +0500

    add dataTables and multiSelect feature

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 
 const Dashboard = React.lazy(() => import('./views/dashboard/Dashboard'))
+const Lodgings = React.lazy(() => import('./views/lodgings/Lodgings'))
+const Countries = React.lazy(() => import('./views/countries/Countries'))
 
 const routes = [
   { path: '/', exact: true, name: 'Home' },
   { path: '/dashboard', name: 'Dashboard', element: Dashboard },
+  { path: '/lodgings', name: 'Lodgings', element: Lodgings },
+  { path: '/countries', name: 'Countries', element: Countries },
 ]
 
 export default routes
