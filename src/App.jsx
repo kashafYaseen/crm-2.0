1aee12de779cd23e3da225c892ac6d1dc3c57887
Kashaf Yaseen <yaseenkashaf9@gmail.com>
2023-07-31 12:32:11 +0500

    Fix private routes for business-owner

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React, { Suspense } from 'react'
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
 import '@/scss/style.scss'
 import LoginAdminUser from '@admin_user_views/sessions/Login'
+import LoginBusinessOwner from '@business_owner_views/sessions/Login'
 import Page404 from '@/viewsTemp/pages/page404/Page404'
 import { useStores } from '@/context/storeContext'
 import { CSpinner } from '@coreui/react'
 import AdminDefaultLayout from '@/layout/admin_user/DefaultLayout'
+import OwnerDefaultLayout from '@/layout/business_owner/DefaultLayout'
 
 const App = () => {
   const authStore = useStores()
@@ -20,11 +22,31 @@ const App = () => {
               !isLoggedIn ? <LoginAdminUser /> : <Navigate replace to={'/admin-user/dashboard'} />
             }
           />
+          <Route
+            path="/business-owner-login"
+            element={
+              !isLoggedIn ? (
+                <LoginBusinessOwner />
+              ) : (
+                <Navigate replace to={'/business-owner/dashboard'} />
+              )
+            }
+          />
 
           <Route
             path="/admin-user/*"
             element={
               !isLoggedIn ? <Navigate replace to={'/admin-user-login'} /> : <AdminDefaultLayout />
+            }
+          />
+          <Route
+            path="/business-owner/*"
+            element={
+              !isLoggedIn ? (
+                <Navigate replace to={'/business-owner-login'} />
+              ) : (
+                <OwnerDefaultLayout />
+              )
             }
           />
 
