1c6a9d8ed2d9b12a93ba743b472698d2153aa8c6
Kashaf Yaseen <yaseenkashaf9@gmail.com>
2023-08-15 15:59:10 +0500

    86857uchr-Implement exact-partner-accounts

---
+++ src/views/admin_user/partners/exact_partner_account/Index.jsx
@@ -0,0 +1,135 @@
+import React, { useEffect, useState } from 'react'
+import 'react-flexy-table/dist/index.css'
+import '@/scss/_custom.scss'
+import { Link } from 'react-router-dom'
+import { Toast } from '@admin_user_components/UI/Toast'
+
+import { DataTable } from '@admin_user_components/UI/DataTable'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEdit } from '@fortawesome/free-solid-svg-icons'
+import { CSpinner, CButton } from '@coreui/react'
+import { useStores } from '@/context/storeContext'
+import { observer } from 'mobx-react'
+import { partners_data } from '@/api/admin_user/config/resources/partners'
+import i18next from 'i18next'
+import { useTranslation } from 'react-i18next'
+
+const ExactPartners = observer(() => {
+  const authStore = useStores()
+  const [partners, setPartners] = useState([])
+  const [totalRecords, setTotalRecords] = useState(0)
+  const [perPageNumber, setPerPageNumber] = useState(10)
+  const [loading, setLoading] = useState(true)
+  const authToken = authStore((state) => state.token)
+  const [showToast, setShowToast] = useState(false)
+  const [error, setError] = useState('')
+  const [errorType, setErrorType] = useState('')
+  const { t } = useTranslation()
+
+  const fetchPartners = async (pageNumber) => {
+    setLoading(true)
+    try {
+      const { data, totalRecords } = await partners_data(
+        'GET',
+        'owners/exact_partner_accounts',
+        null,
+        {
+          page: pageNumber,
+          per_page: perPageNumber,
+          query: searchQuery,
+        },
+        authToken,
+      )
+
+      setPartners(data)
+      setTotalRecords(totalRecords)
+      setLoading(false)
+    } catch (error) {
+      setLoading(false)
+      throw error
+    }
+  }
+
+  useEffect(() => {
+    fetchPartners()
+  }, [perPageNumber])
+
+  const handleToastHide = () => {
+    setShowToast(false)
+  }
+
+  const [searchQuery, setSearchQuery] = useState('')
+
+  const columns = [
+    { header: t('name'), key: 'full_name', td: (row) => row.full_name ?? '' },
+    { header: t('business_name'), key: 'business_name', td: (row) => row.business_name ?? '' },
+    { header: t('account_id'), key: 'account_id', td: (row) => row.account_id ?? '' },
+    {
+      header: 'Actions',
+      td: (row) => (
+        <>
+          <Link
+            to={`/${i18next.language}/admin-user/edit`}
+            state={{ record: row }}
+            className="custom-link"
+          >
+            <FontAwesomeIcon icon={faEdit} />
+          </Link>
+        </>
+      ),
+    },
+  ]
+
+  const handleSearchInputChange = (event) => {
+    setSearchQuery(event.target.value)
+  }
+
+  const searchQueryHandler = async () => {
+    fetchPartners()
+  }
+
+  const onPageChangeHandler = (value) => {
+    fetchPartners(value)
+  }
+
+  const onPerPageChangeHandler = (value) => {
+    setPerPageNumber(value)
+  }
+
+  return (
+    <div className="display">
+      <div className="toast-container">
+        {showToast && <Toast error={error} onExited={handleToastHide} type={errorType} />}
+      </div>
+      <h2 className="mb-3">{t('exact_partner_account')}</h2>
+      <div className="custom-search-container ">
+        <input
+          type="text"
+          value={searchQuery}
+          onChange={handleSearchInputChange}
+          placeholder="Search..."
+          className="custom-search-input"
+        />
+        <button className="create-button" onClick={searchQueryHandler}>
+          {t('search')}
+        </button>
+      </div>
+
+      {partners.length > 0 ? (
+        <DataTable
+          data={partners}
+          columns={columns}
+          totalRecords={totalRecords}
+          onPageChange={onPageChangeHandler}
+          onPerPageChange={onPerPageChangeHandler}
+          perPageNumber={perPageNumber}
+          loading={loading}
+        />
+      ) : (
+        <div>{loading ? <CSpinner color="secondary" variant="grow" /> : 'No records found'}</div>
+      )}
+    </div>
+  )
+})
+
+export default ExactPartners
